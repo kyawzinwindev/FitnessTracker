683c1475cf1605151574299d23c2c67a8e24739b
Kyaw Zin Win <kyawzinw469@gmail.com>
2025-07-03 11:54:20 +0700

    feat: add Goal class

---
+++ Goal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker
+{
+    internal class Goal
+    {
+        protected int userID;
+        protected double targetCalories;
+        protected DateTime startDate, endDate;
+        protected bool isGoalAchieved;
+
+        public int UserID
+        {
+            get { return userID; }
+            set { userID = value; }
+        }
+
+        public double TargetCalories
+        {
+            get { return targetCalories; }
+            set { targetCalories = value; }
+        }
+
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { startDate = value; }
+        }
+
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { endDate = value; }
+        }
+
+        public bool IsGoalAchieved
+        {
+            get { return isGoalAchieved; }
+            set { isGoalAchieved = value; }
+        }
+    }
+}
